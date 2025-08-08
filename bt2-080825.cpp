6ac2531b789048aa8a95133b40e1c803e9618a3d
khoalez <lehuynhdangkhoa12@gmail.com>
2025-08-08 08:15:50 +0700

    Add files via upload

---
+++ bt2-080825.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int a, b;
+int main() {
+    cin >> a >> b;
+    if (a >= b)
+        cout << a << b;
+    else
+        cout << b << a;
+return 0;
+}
